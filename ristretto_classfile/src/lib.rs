6524746ad6f13a8711e6719a56e407ebcfb81272
brianheineman <brian.heineman@gmail.com>
2024-07-08 19:08:11 -0600

    docs: add library documentation

--- ristretto_classfile/src/lib.rs
+++ ristretto_classfile/src/lib.rs
@@ -1,3 +1,52 @@
+//! # Ristretto ClassFile
+//!
+//! [![Code Coverage](https://codecov.io/gh/theseus-rs/ristretto/branch/main/graph/badge.svg)](https://codecov.io/gh/theseus-rs/ristretto)
+//! [![Benchmarks](https://img.shields.io/badge/%F0%9F%90%B0_bencher-enabled-6ec241)](https://bencher.dev/perf/theseus-rs-ristretto)
+//! [![License](https://img.shields.io/crates/l/ristretto_classfile)](https://github.com/theseus-rs/ristretto_classfile#license)
+//! [![Semantic Versioning](https://img.shields.io/badge/%E2%9A%99%EF%B8%8F_SemVer-2.0.0-blue)](https://semver.org/spec/v2.0.0.html)
+//!
+//! ## Getting Started
+//!
+//! Implementation of the [JVM class File Format](https://docs.oracle.com/javase/specs/jvms/se22/html/jvms-4.html) that
+//! is used to read, write and verify Java classes.
+//!
+//! Reading and writing class files for any version of Java version up to 23. Verification of class files is supported, but
+//! is still a work in progress. The API is currently unstable and will change as the implementation progresses.
+//!
+//! # Examples
+//!
+//! ```rust
+//! use ristretto_classfile::{ClassFile, Constant, ConstantPool, Result, Version};
+//!
+//! fn main() -> Result<()> {
+//!     let mut constant_pool = ConstantPool::default();
+//!     constant_pool.add(Constant::Utf8("Foo".to_string()));
+//!     let utf8_index = u16::try_from(constant_pool.len())?;
+//!     constant_pool.add(Constant::Class {
+//!         name_index: utf8_index,
+//!     });
+//!     let class_index = u16::try_from(constant_pool.len())?;
+//!     let class_file = ClassFile {
+//!         version: Version::Java21 { minor: 0 },
+//!         constant_pool,
+//!         this_class: class_index,
+//!         ..Default::default()
+//!     };
+//!     class_file.verify()
+//! }
+//! ```
+//!
+//! ## Safety
+//!
+//! These crates use `#![forbid(unsafe_code)]` to ensure everything is implemented in 100% safe Rust.
+//!
+//! ## License
+//!
+//! Licensed under either of
+//!
+//! * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or https://www.apache.org/licenses/LICENSE-2.0)
+//! * MIT license ([LICENSE-MIT](LICENSE-MIT) or https://opensource.org/licenses/MIT)
+
 #![forbid(unsafe_code)]
 #![allow(dead_code)]
 #![deny(clippy::pedantic)]
