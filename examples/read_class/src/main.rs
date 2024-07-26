0327b0c656a8b03dca3aaf4d62eda9175a62a5fc
brianheineman <brian.heineman@gmail.com>
2024-07-26 06:46:15 -0600

    test: improve code coverage for read_class example

--- examples/read_class/src/main.rs
+++ examples/read_class/src/main.rs
@@ -1,28 +1,28 @@
 #![forbid(unsafe_code)]
 #![deny(clippy::pedantic)]
 
-use ristretto_classfile::{ClassFile, Result};
+use anyhow::{bail, Result};
+use ristretto_classfile::ClassFile;
+use std::env;
 use std::fs;
 use std::io::Cursor;
-use std::{env, process};
 
 /// Example that reads a class file and verifies it.
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    process::exit(run(&args)?)
+    run(&args)
 }
 
-fn run(args: &[String]) -> Result<i32> {
+fn run(args: &[String]) -> Result<()> {
     if args.len() != 2 {
-        eprintln!("Usage: {} <file_name>", args[0]);
-        return Ok(1);
+        bail!("Usage: {} <file_name>", args[0]);
     }
     let file_name = &args[1];
     let bytes = fs::read(file_name)?;
     let class_file = ClassFile::from_bytes(&mut Cursor::new(bytes))?;
     class_file.verify()?;
     println!("{class_file}");
-    Ok(0)
+    Ok(())
 }
 
 #[cfg(test)]
@@ -35,16 +35,12 @@ mod test {
             "read_class".to_string(),
             "classes/HelloWorld.class".to_string(),
         ];
-        let exit_code = run(&args)?;
-        assert_eq!(0, exit_code);
-        Ok(())
+        run(&args)
     }
 
     #[test]
-    fn test_main_error() -> Result<()> {
+    fn test_main_error() {
         let args = vec!["read_class".to_string()];
-        let exit_code = run(&args)?;
-        assert_eq!(1, exit_code);
-        Ok(())
+        assert!(run(&args).is_err());
     }
 }
