b5d064e58070737ce19e3c8aa60b8eb7f841b6e3
brianheineman <brian.heineman@gmail.com>
2024-07-27 15:36:16 -0600

    test: add deserialization fuzz testing

---
+++ fuzz/fuzz_targets/classfile/attributes/line_number_from_bytes.rs
@@ -0,0 +1,12 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+
+use ristretto_classfile::attributes::LineNumber;
+use std::io::Cursor;
+
+fuzz_target!(|data: &[u8]| {
+    let data = data.to_vec();
+    let mut cursor = Cursor::new(data);
+    let _ = LineNumber::from_bytes(&mut cursor);
+});
---
+++ fuzz/fuzz_targets/classfile/attributes/record_from_bytes.rs
@@ -0,0 +1,14 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+
+use ristretto_classfile::attributes::Record;
+use ristretto_classfile::ConstantPool;
+use std::io::Cursor;
+
+fuzz_target!(|data: &[u8]| {
+    let data = data.to_vec();
+    let mut cursor = Cursor::new(data);
+    let constant_pool = ConstantPool::default();
+    let _ = Record::from_bytes(&constant_pool, &mut cursor);
+});
