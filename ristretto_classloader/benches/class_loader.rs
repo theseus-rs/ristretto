2d8fdf38f941b5266239fa3c71369dc14b852f47
brianheineman <brian.heineman@gmail.com>
2024-08-15 17:49:53 -0600

    test: add class loader benchmarks

--- ristretto_classloader/benches/class_loader.rs
+++ ristretto_classloader/benches/class_loader.rs
@@ -10,6 +10,9 @@ fn benchmarks(criterion: &mut Criterion) {
 
 fn bench_lifecycle(criterion: &mut Criterion) -> Result<()> {
     let runtime = Runtime::new().unwrap();
+    let (_version, class_loader) =
+        runtime.block_on(async { runtime::class_loader("21.0.4.7.1").await })?;
+    let class_loader = Arc::new(class_loader);
 
     criterion.bench_function("runtime_v8", |bencher| {
         bencher.iter(|| {
@@ -36,6 +39,22 @@ fn bench_lifecycle(criterion: &mut Criterion) -> Result<()> {
         bencher.iter(|| {
             runtime.block_on(async {
                 runtime_class_loader("21.0.4.7.1").await.ok();
+            });
+        });
+    });
+    criterion.bench_function("load_hash_map", |bencher| {
+        bencher.iter(|| {
+            runtime.block_on(async {
+                let _ = ClassLoader::load_class(&class_loader, "java.util.HashMap")
+                    .await
+                    .ok();
+            });
+        });
+    });
+    criterion.bench_function("load_invalid_class", |bencher| {
+        bencher.iter(|| {
+            runtime.block_on(async {
+                let _ = ClassLoader::load_class(&class_loader, "foo").await.err();
             });
         });
     });
