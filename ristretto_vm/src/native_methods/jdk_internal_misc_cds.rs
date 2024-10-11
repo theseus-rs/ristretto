11da2f7a32cb32ff83d817b78e89e4f86ac57845
brianheineman <brian.heineman@gmail.com>
2024-10-11 17:09:31 -0600

    feat: implement native method CDS.getRandomSeedForDumping()J

--- ristretto_vm/src/native_methods/jdk_internal_misc_cds.rs
+++ ristretto_vm/src/native_methods/jdk_internal_misc_cds.rs
@@ -3,10 +3,17 @@ use crate::call_stack::CallStack;
 use crate::native_methods::registry::MethodRegistry;
 use crate::{Result, VM};
 use ristretto_classloader::Value;
+use std::hash::{DefaultHasher, Hash, Hasher};
 
 /// Register all native methods for jdk.internal.misc.CDS.
 pub(crate) fn register(registry: &mut MethodRegistry) {
     let class_name = "jdk/internal/misc/CDS";
+    registry.register(
+        class_name,
+        "getRandomSeedForDumping",
+        "()J",
+        get_random_seed_for_dumping,
+    );
     registry.register(
         class_name,
         "initializeFromArchive",
@@ -21,6 +28,21 @@ pub(crate) fn register(registry: &mut MethodRegistry) {
         is_dumping_class_list_0,
     );
     registry.register(class_name, "isSharingEnabled0", "()Z", is_sharing_enabled_0);
+}
+
+#[expect(clippy::cast_possible_wrap)]
+#[expect(clippy::needless_pass_by_value)]
+#[expect(clippy::unnecessary_wraps)]
+fn get_random_seed_for_dumping(
+    _vm: &VM,
+    _call_stack: &mut CallStack,
+    _arguments: Arguments,
+) -> Result<Option<Value>> {
+    let version = env!("CARGO_PKG_VERSION");
+    let mut hasher = DefaultHasher::new();
+    version.hash(&mut hasher);
+    let hash = hasher.finish() as i64;
+    Ok(Some(Value::Long(hash)))
 }
 
 fn initialize_from_archive(
