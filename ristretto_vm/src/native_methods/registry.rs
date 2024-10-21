134564877fb508df152234bafcee70bc9f5180f3
brianheineman <brian.heineman@gmail.com>
2024-10-20 22:27:07 -0600

    fix: implement java.io.WinNTFileSystem.initIDs()

--- ristretto_vm/src/native_methods/registry.rs
+++ ristretto_vm/src/native_methods/registry.rs
@@ -2,13 +2,14 @@ use crate::arguments::Arguments;
 use crate::call_stack::CallStack;
 use crate::native_methods::{
     java_io_filedescriptor, java_io_fileinputstream, java_io_fileoutputstream,
-    java_io_unixfilesystem, java_lang_class, java_lang_classloader, java_lang_double,
-    java_lang_float, java_lang_object, java_lang_ref_reference, java_lang_runtime,
-    java_lang_shutdown, java_lang_stacktraceelement, java_lang_system, java_lang_thread,
-    java_lang_throwable, jdk_internal_loader_bootloader, jdk_internal_loader_nativelibraries,
-    jdk_internal_misc_cds, jdk_internal_misc_scopedmemoryaccess, jdk_internal_misc_signal,
-    jdk_internal_misc_unsafe, jdk_internal_misc_vm, jdk_internal_reflect_reflection,
-    jdk_internal_util_systemprops_raw, sun_io_win32errormode, sun_nio_fs_unixnativedispatcher,
+    java_io_unixfilesystem, java_io_winntfilesystem, java_lang_class, java_lang_classloader,
+    java_lang_double, java_lang_float, java_lang_object, java_lang_ref_reference,
+    java_lang_runtime, java_lang_shutdown, java_lang_stacktraceelement, java_lang_system,
+    java_lang_thread, java_lang_throwable, jdk_internal_loader_bootloader,
+    jdk_internal_loader_nativelibraries, jdk_internal_misc_cds,
+    jdk_internal_misc_scopedmemoryaccess, jdk_internal_misc_signal, jdk_internal_misc_unsafe,
+    jdk_internal_misc_vm, jdk_internal_reflect_reflection, jdk_internal_util_systemprops_raw,
+    sun_io_win32errormode, sun_nio_fs_unixnativedispatcher,
 };
 use crate::Result;
 use ristretto_classloader::Value;
@@ -87,6 +88,7 @@ impl Default for MethodRegistry {
         java_io_fileinputstream::register(&mut registry);
         java_io_fileoutputstream::register(&mut registry);
         java_io_unixfilesystem::register(&mut registry);
+        java_io_winntfilesystem::register(&mut registry);
         java_lang_class::register(&mut registry);
         java_lang_classloader::register(&mut registry);
         java_lang_double::register(&mut registry);
