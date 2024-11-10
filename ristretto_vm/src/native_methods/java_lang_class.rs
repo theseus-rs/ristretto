e9582041727297ada1fd411760745f1918bab4bd
brianheineman <brian.heineman@gmail.com>
2024-11-10 10:47:55 -0700

    fix: add Class.isInterface()Z

--- ristretto_vm/src/native_methods/java_lang_class.rs
+++ ristretto_vm/src/native_methods/java_lang_class.rs
@@ -3,9 +3,9 @@ use crate::java_object::JavaObject;
 use crate::native_methods::registry::MethodRegistry;
 use crate::thread::Thread;
 use crate::Error::{InternalError, NullPointer};
-use crate::Result;
+use crate::{Result, VM};
 use async_recursion::async_recursion;
-use ristretto_classloader::{Reference, Value};
+use ristretto_classloader::{Class, Object, Reference, Value};
 use std::sync::Arc;
 
 /// Register all native methods for java.lang.Class.
@@ -36,8 +36,19 @@ pub(crate) fn register(registry: &mut MethodRegistry) {
         "(Ljava/lang/Class;)Z",
         is_assignable_from,
     );
+    registry.register(class_name, "isInterface", "()Z", is_interface);
     registry.register(class_name, "isPrimitive", "()Z", is_primitive);
     registry.register(class_name, "registerNatives", "()V", register_natives);
+}
+
+async fn get_class(vm: &VM, object: &Object) -> Result<Arc<Class>> {
+    let class = object.class();
+    if class.name() == "java/lang/Class" {
+        let class_name: String = object.value("name")?.try_into()?;
+        let class = vm.class(class_name.as_str()).await?;
+        return Ok(class);
+    }
+    Ok(Arc::clone(class))
 }
 
 #[expect(clippy::needless_pass_by_value)]
@@ -83,13 +94,13 @@ async fn get_super_class(thread: Arc<Thread>, mut arguments: Arguments) -> Resul
     }
 }
 
-#[expect(clippy::needless_pass_by_value)]
 #[async_recursion(?Send)]
-async fn is_array(_thread: Arc<Thread>, mut arguments: Arguments) -> Result<Option<Value>> {
+async fn is_array(thread: Arc<Thread>, mut arguments: Arguments) -> Result<Option<Value>> {
     let Some(Reference::Object(object)) = arguments.pop_object()? else {
         return Err(InternalError("isArray: no arguments".to_string()));
     };
-    let class = object.class();
+    let vm = thread.vm()?;
+    let class = get_class(&vm, &object).await?;
     if class.is_array() {
         Ok(Some(Value::Int(1)))
     } else {
@@ -97,10 +108,9 @@ async fn is_array(_thread: Arc<Thread>, mut arguments: Arguments) -> Result<Opti
     }
 }
 
-#[expect(clippy::needless_pass_by_value)]
 #[async_recursion(?Send)]
 async fn is_assignable_from(
-    _thread: Arc<Thread>,
+    thread: Arc<Thread>,
     mut arguments: Arguments,
 ) -> Result<Option<Value>> {
     let object_argument = match arguments.pop_object()? {
@@ -108,31 +118,44 @@ async fn is_assignable_from(
         None => return Err(NullPointer("object cannot be null".to_string())),
         _ => return Err(InternalError("isAssignableFrom: no arguments".to_string())),
     };
-    let class_argument = object_argument.class();
+    let vm = thread.vm()?;
+    let class_argument = get_class(&vm, &object_argument).await?;
     let Some(Reference::Object(object)) = arguments.pop_object()? else {
         return Err(InternalError("isAssignableFrom: no instance".to_string()));
     };
-    let class = object.class();
-    if class.is_assignable_from(class_argument)? {
+    let class = get_class(&vm, &object).await?;
+    if class.is_assignable_from(&class_argument)? {
         Ok(Some(Value::Int(1)))
     } else {
         Ok(Some(Value::Int(0)))
     }
 }
 
-#[expect(clippy::needless_pass_by_value)]
 #[async_recursion(?Send)]
-async fn is_primitive(_thread: Arc<Thread>, mut arguments: Arguments) -> Result<Option<Value>> {
+async fn is_interface(thread: Arc<Thread>, mut arguments: Arguments) -> Result<Option<Value>> {
     let Some(Reference::Object(object)) = arguments.pop_object()? else {
         return Err(InternalError("isPrimitive: no arguments".to_string()));
     };
-    let class_name: String = object.value("name")?.try_into()?;
-    let class_name = class_name.as_str();
-    match class_name {
-        "boolean" | "byte" | "char" | "double" | "float" | "int" | "long" | "short" | "void" => {
-            Ok(Some(Value::Int(1)))
-        }
-        _ => Ok(Some(Value::Int(0))),
+    let vm = thread.vm()?;
+    let class = get_class(&vm, &object).await?;
+    if class.is_interface() {
+        Ok(Some(Value::Int(1)))
+    } else {
+        Ok(Some(Value::Int(0)))
+    }
+}
+
+#[async_recursion(?Send)]
+async fn is_primitive(thread: Arc<Thread>, mut arguments: Arguments) -> Result<Option<Value>> {
+    let Some(Reference::Object(object)) = arguments.pop_object()? else {
+        return Err(InternalError("isPrimitive: no arguments".to_string()));
+    };
+    let vm = thread.vm()?;
+    let class = get_class(&vm, &object).await?;
+    if class.is_primitive() {
+        Ok(Some(Value::Int(1)))
+    } else {
+        Ok(Some(Value::Int(0)))
     }
 }
 
