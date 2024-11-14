5104b34555e2dfc89261409ae676121e41f11c5c
brianheineman <brian.heineman@gmail.com>
2024-11-14 10:18:01 -0700

    fix: add jdk.internal.misc.CDS.getCDSConfigStatus()I

--- ristretto_vm/src/native_methods/jdk_internal_misc_cds.rs
+++ ristretto_vm/src/native_methods/jdk_internal_misc_cds.rs
@@ -12,6 +12,12 @@ pub(crate) fn register(registry: &mut MethodRegistry) {
     let class_name = "jdk/internal/misc/CDS";
     registry.register(
         class_name,
+        "getCDSConfigStatus",
+        "()I",
+        get_cds_config_status,
+    );
+    registry.register(
+        class_name,
         "getRandomSeedForDumping",
         "()J",
         get_random_seed_for_dumping,
@@ -30,6 +36,15 @@ pub(crate) fn register(registry: &mut MethodRegistry) {
         is_dumping_class_list_0,
     );
     registry.register(class_name, "isSharingEnabled0", "()Z", is_sharing_enabled_0);
+}
+
+#[expect(clippy::needless_pass_by_value)]
+#[async_recursion(?Send)]
+async fn get_cds_config_status(
+    _thread: Arc<Thread>,
+    _arguments: Arguments,
+) -> Result<Option<Value>> {
+    Ok(Some(Value::Int(0)))
 }
 
 #[expect(clippy::cast_possible_wrap)]
