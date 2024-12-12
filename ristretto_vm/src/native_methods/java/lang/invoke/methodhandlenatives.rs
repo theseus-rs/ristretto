1c0f4ccbbc9821473596b65f23cd0423e2be9a9e
brianheineman <brian.heineman@gmail.com>
2024-12-12 14:07:28 -0700

    fix: implement java.lang.invoke.MethodHandleNatives.resolve(Ljava/lang/invoke/MemberName;Ljava/lang/Class;IZ)Ljava/lang/invoke/MemberName;

--- ristretto_vm/src/native_methods/java/lang/invoke/methodhandlenatives.rs
+++ ristretto_vm/src/native_methods/java/lang/invoke/methodhandlenatives.rs
@@ -4,7 +4,7 @@ use crate::thread::Thread;
 use crate::Result;
 use async_recursion::async_recursion;
 use ristretto_classfile::Version;
-use ristretto_classloader::{Class, Value};
+use ristretto_classloader::{Class, Object, Value};
 use std::sync::Arc;
 
 const JAVA_8: Version = Version::Java8 { minor: 0 };
@@ -22,7 +22,7 @@ pub(crate) fn register(registry: &mut MethodRegistry) {
             class_name,
             "resolve",
             "(Ljava/lang/invoke/MemberName;Ljava/lang/Class;)Ljava/lang/invoke/MemberName;",
-            resolve,
+            resolve_0,
         );
     } else {
         registry.register(
@@ -41,7 +41,7 @@ pub(crate) fn register(registry: &mut MethodRegistry) {
             class_name,
             "resolve",
             "(Ljava/lang/invoke/MemberName;Ljava/lang/Class;Z)Ljava/lang/invoke/MemberName;",
-            resolve,
+            resolve_1,
         );
     }
 
@@ -50,7 +50,7 @@ pub(crate) fn register(registry: &mut MethodRegistry) {
             class_name,
             "resolve",
             "(Ljava/lang/invoke/MemberName;Ljava/lang/Class;IZ)Ljava/lang/invoke/MemberName;",
-            resolve,
+            resolve_2,
         );
     }
 
@@ -171,13 +171,82 @@ async fn register_natives(_thread: Arc<Thread>, _arguments: Arguments) -> Result
     Ok(None)
 }
 
+async fn resolve(
+    thread: Arc<Thread>,
+    member_self: Object,
+    _caller: Arc<Class>,
+    _lookup_mode: i32,
+    _speculative_resolve: bool,
+) -> Result<Option<Value>> {
+    let class_object: Object = member_self.value("clazz")?.try_into()?;
+    let class_name: String = class_object.value("name")?.try_into()?;
+    let class = thread.class(class_name).await?;
+    let name: String = member_self.value("name")?.try_into()?;
+
+    let method_type = member_self.value("type")?;
+    let method_type_class = thread.class("java.lang.invoke.MethodType").await?;
+    let method_type_descriptor_method =
+        method_type_class.try_get_method("toMethodDescriptorString", "()Ljava/lang/String;")?;
+    let descriptor: String = thread
+        .try_execute(
+            &method_type_class,
+            &method_type_descriptor_method,
+            vec![method_type],
+            true,
+        )
+        .await?
+        .try_into()?;
+
+    // Lookup modes:
+    // LM_MODULE = 16;
+    // LM_UNCONDITIONAL = 32;
+    // LM_TRUSTED = -1;
+
+    let method = class.try_get_method(name, descriptor)?;
+    let access_flags = i32::from(method.access_flags().bits());
+    let flags: i32 = member_self.value("flags")?.try_into()?;
+    let flags = flags | access_flags;
+    member_self.set_value("flags", Value::from(flags))?;
+
+    let resolved_method_name_class = thread.class("java.lang.invoke.ResolvedMethodName").await?;
+    let resolved_method_name = Object::new(resolved_method_name_class)?;
+    // TODO: the ResolvedMethodName class appears to be a holder for the JVM-internal class and
+    // method references.  This should evaluated to determine if it is necessary to implement.
+    member_self.set_value("method", Value::from(resolved_method_name))?;
+
+    let member_self = Value::from(member_self);
+    Ok(Some(member_self))
+}
+
 #[async_recursion(?Send)]
-async fn resolve(_thread: Arc<Thread>, mut arguments: Arguments) -> Result<Option<Value>> {
-    let _speculative_resolve = arguments.pop_int()? != 0;
-    let _lookup_mode = arguments.pop_int()?;
-    let _caller: Arc<Class> = arguments.pop_object()?.try_into()?;
+async fn resolve_0(thread: Arc<Thread>, mut arguments: Arguments) -> Result<Option<Value>> {
+    let caller: Arc<Class> = arguments.pop_object()?.try_into()?;
     let member_self = arguments.pop_object()?;
-    Ok(Some(Value::from(member_self)))
+    resolve(thread, member_self, caller, -1, true).await
+}
+
+#[async_recursion(?Send)]
+async fn resolve_1(thread: Arc<Thread>, mut arguments: Arguments) -> Result<Option<Value>> {
+    let speculative_resolve = arguments.pop_int()? != 0;
+    let caller: Arc<Class> = arguments.pop_object()?.try_into()?;
+    let member_self = arguments.pop_object()?;
+    resolve(thread, member_self, caller, -1, speculative_resolve).await
+}
+
+#[async_recursion(?Send)]
+async fn resolve_2(thread: Arc<Thread>, mut arguments: Arguments) -> Result<Option<Value>> {
+    let speculative_resolve = arguments.pop_int()? != 0;
+    let lookup_mode = arguments.pop_int()?;
+    let caller: Arc<Class> = arguments.pop_object()?.try_into()?;
+    let member_self = arguments.pop_object()?;
+    resolve(
+        thread,
+        member_self,
+        caller,
+        lookup_mode,
+        speculative_resolve,
+    )
+    .await
 }
 
 #[async_recursion(?Send)]
