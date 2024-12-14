30d8c9eaad1d774128195753020056450178f946
brianheineman <brian.heineman@gmail.com>
2024-12-13 21:10:16 -0700

    chore: display JAVA_VERSION or default java version for the CLI --version option

--- ristretto_cli/src/version.rs
+++ ristretto_cli/src/version.rs
@@ -1,13 +1,17 @@
+use ristretto_vm::DEFAULT_JAVA_VERSION;
+use std::env;
+
 /// Get the full version of the program (e.g. "java/0.0.0 Linux/5.11.0-37-generic/x86_64").
 pub fn full() -> String {
     let program_name = "java";
     let version = env!("CARGO_PKG_VERSION");
+    let java_version = env::var("JAVA_VERSION").unwrap_or(DEFAULT_JAVA_VERSION.to_string());
     let info = os_info::get();
     let os = format!("{}", info.os_type()).replace(' ', "-");
     let os_version = info.version();
     let architecture = info.architecture().unwrap_or("unknown");
 
-    format!("{program_name}/{version} {os}/{os_version}/{architecture}")
+    format!("{program_name}/{version}/{java_version} {os}/{os_version}/{architecture}")
 }
 
 #[cfg(test)]
