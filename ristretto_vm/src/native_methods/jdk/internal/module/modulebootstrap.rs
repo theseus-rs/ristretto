ef3b3483d3b1e3fe4de728f5f7b3bebe9a7c5ff2
brianheineman <brian.heineman@gmail.com>
2025-03-03 13:56:11 -0700

    fix: return stub boot ModuleLayer

--- ristretto_vm/src/native_methods/jdk/internal/module/modulebootstrap.rs
+++ ristretto_vm/src/native_methods/jdk/internal/module/modulebootstrap.rs
@@ -14,20 +14,35 @@ pub(crate) fn register(registry: &mut MethodRegistry) {
 }
 
 #[async_recursion(?Send)]
-async fn boot(_thread: Arc<Thread>, _parameters: Parameters) -> Result<Option<Value>> {
-    // TODO: remove this method once the module system is implemented
-    Ok(Some(Value::Object(None)))
+async fn boot(thread: Arc<Thread>, _parameters: Parameters) -> Result<Option<Value>> {
+    // TODO: correctly populate the ModuleLayer
+    let empty_list = thread
+        .object("java/util/ArrayList", "", Vec::<Value>::new())
+        .await?;
+    let module_layer = thread
+        .object(
+            "java/lang/ModuleLayer",
+            "Ljava/lang/module/Configuration;Ljava/util/List;Ljava/util/function/Function;",
+            vec![Value::Object(None), empty_list, Value::Object(None)],
+        )
+        .await?;
+    Ok(Some(module_layer))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ristretto_classloader::Object;
 
     #[tokio::test]
     async fn test_boot() -> Result<()> {
         let (_vm, thread) = crate::test::thread().await?;
-        let result = boot(thread, Parameters::default()).await?;
-        assert_eq!(result, Some(Value::Object(None)));
+        let result = boot(thread, Parameters::default())
+            .await?
+            .expect("module layer");
+        let object: Object = result.try_into()?;
+        let class = object.class();
+        assert_eq!("java/lang/ModuleLayer", class.name());
         Ok(())
     }
 }
