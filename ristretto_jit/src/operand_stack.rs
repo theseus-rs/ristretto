5d543f558bea82ada82b26185c761bd279a8652e
brianheineman <brian.heineman@gmail.com>
2025-05-10 13:02:35 -0600

    feat: optimize operand stack layout

--- ristretto_jit/src/operand_stack.rs
+++ ristretto_jit/src/operand_stack.rs
@@ -2,11 +2,20 @@ use crate::Error::InternalError;
 use crate::Result;
 use cranelift::codegen::ir::{StackSlot, Value};
 use cranelift::frontend::FunctionBuilder;
-use cranelift::prelude::{InstBuilder, StackSlotData, StackSlotKind, types};
+use cranelift::prelude::{InstBuilder, StackSlotData, StackSlotKind, Type, types};
 
 const POINTER_SIZE: i32 = 8;
 
 /// Operand stack for the JIT compiler.
+///
+/// The stack is implemented as a continuous array of 64bit/8byte values.  I32 and F32 values are
+/// padded to 8 bytes.  The stack is aligned to 8 bytes for performance reasons.
+///
+/// ```text
+/// |------- 8 bytes -------|----- 8 bytes -----|------- 8 bytes -------|----- 8 bytes -----|
+/// |- 4 bytes -|- 4 bytes -|                   |- 4 bytes -|- 4 bytes -|                   |
+/// |  padding  |    I32    |        I64        |  padding  |    F32    |        F64        |
+/// ```
 pub struct OperandStack {
     slot: Option<StackSlot>,
     length: i32,
@@ -32,7 +41,12 @@ impl OperandStack {
     }
 
     /// Pushes a value onto the stack.
-    pub fn push(&mut self, function_builder: &mut FunctionBuilder, value: Value) -> Result<()> {
+    fn push_type(
+        &mut self,
+        function_builder: &mut FunctionBuilder,
+        value_type: Type,
+        value: Value,
+    ) -> Result<()> {
         let Some(slot) = self.slot else {
             return Err(InternalError(
                 "OperandStack.slot is not initialized".to_string(),
@@ -41,14 +55,22 @@ impl OperandStack {
         let index = self.length;
         self.length = index + 1;
         let slot_index = index.saturating_mul(POINTER_SIZE);
+        let value_type_bytes = value_type.bytes();
+        let padding = POINTER_SIZE.saturating_sub_unsigned(value_type_bytes);
+        let slot_index = slot_index.saturating_add(padding);
         function_builder.ins().stack_store(value, slot, slot_index);
         Ok(())
     }
 
+    /// Pushes a value onto the stack.
+    pub fn push(&mut self, function_builder: &mut FunctionBuilder, value: Value) -> Result<()> {
+        // Default to I64/8-bytes.
+        self.push_type(function_builder, types::I64, value)
+    }
+
     /// Push an int value onto the operand stack.
     pub fn push_int(&mut self, function_builder: &mut FunctionBuilder, value: Value) -> Result<()> {
-        let value = function_builder.ins().sextend(types::I64, value);
-        self.push(function_builder, value)
+        self.push_type(function_builder, types::I32, value)
     }
 
     /// Push a long value onto the operand stack.
@@ -57,7 +79,7 @@ impl OperandStack {
         function_builder: &mut FunctionBuilder,
         value: Value,
     ) -> Result<()> {
-        self.push(function_builder, value)
+        self.push_type(function_builder, types::I64, value)
     }
 
     /// Push a float value onto the operand stack.
@@ -66,8 +88,7 @@ impl OperandStack {
         function_builder: &mut FunctionBuilder,
         value: Value,
     ) -> Result<()> {
-        let value = function_builder.ins().fcvt_to_sint(types::I64, value);
-        self.push(function_builder, value)
+        self.push_type(function_builder, types::F32, value)
     }
 
     /// Push a double value onto the operand stack.
@@ -76,12 +97,15 @@ impl OperandStack {
         function_builder: &mut FunctionBuilder,
         value: Value,
     ) -> Result<()> {
-        let value = function_builder.ins().fcvt_to_sint(types::I64, value);
-        self.push(function_builder, value)
+        self.push_type(function_builder, types::F64, value)
     }
 
     /// Pops a value from the stack.
-    pub fn pop(&mut self, function_builder: &mut FunctionBuilder) -> Result<Value> {
+    fn pop_type(
+        &mut self,
+        function_builder: &mut FunctionBuilder,
+        value_type: Type,
+    ) -> Result<Value> {
         let Some(slot) = self.slot else {
             return Err(InternalError(
                 "OperandStack.slot is not initialized".to_string(),
@@ -89,36 +113,39 @@ impl OperandStack {
         };
         let index = self.length - 1;
         let slot_index = index.saturating_mul(POINTER_SIZE);
+        let value_type_bytes = value_type.bytes();
+        let padding = POINTER_SIZE.saturating_sub_unsigned(value_type_bytes);
+        let slot_index = slot_index.saturating_add(padding);
         let value = function_builder
             .ins()
-            .stack_load(types::I64, slot, slot_index);
+            .stack_load(value_type, slot, slot_index);
         self.length = index;
         Ok(value)
     }
 
+    /// Pops a value from the stack.
+    pub fn pop(&mut self, function_builder: &mut FunctionBuilder) -> Result<Value> {
+        // Default to I64/8-bytes.
+        self.pop_type(function_builder, types::I64)
+    }
+
     /// Pop an int from the operand stack.
     pub fn pop_int(&mut self, function_builder: &mut FunctionBuilder) -> Result<Value> {
-        let value = self.pop(function_builder)?;
-        let value = function_builder.ins().ireduce(types::I32, value);
-        Ok(value)
+        self.pop_type(function_builder, types::I32)
     }
 
     /// Pop a long from the operand stack.
     pub fn pop_long(&mut self, function_builder: &mut FunctionBuilder) -> Result<Value> {
-        self.pop(function_builder)
+        self.pop_type(function_builder, types::I64)
     }
 
     /// Pop a float from the operand stack.
     pub fn pop_float(&mut self, function_builder: &mut FunctionBuilder) -> Result<Value> {
-        let value = self.pop(function_builder)?;
-        let value = function_builder.ins().fcvt_from_sint(types::F32, value);
-        Ok(value)
+        self.pop_type(function_builder, types::F32)
     }
 
     /// Pop a double from the operand stack.
     pub fn pop_double(&mut self, function_builder: &mut FunctionBuilder) -> Result<Value> {
-        let value = self.pop(function_builder)?;
-        let value = function_builder.ins().fcvt_from_sint(types::F64, value);
-        Ok(value)
+        self.pop_type(function_builder, types::F64)
     }
 }
