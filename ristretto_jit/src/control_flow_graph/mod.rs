4c67e99fdec793327eeffe3791878f3c70023634
brianheineman <brian.heineman@gmail.com>
2025-05-21 14:35:23 -0600

    docs: update cfg doc formatting

--- ristretto_jit/src/control_flow_graph/mod.rs
+++ ristretto_jit/src/control_flow_graph/mod.rs
@@ -22,45 +22,45 @@
 /// Control flow graph diagram for `Integer.max(II)I` transformed into Cranelift blocks where the
 /// stack state is managed using SSA by defining arguments on blocks and passing parameters:
 /// ```text
-///                         +------------------------------------------------------------+
-///                         |                         block0 ()                          |
-///                         |  // entry point with 2 local variables (l0: i32, l1: i32)  |
-///                         |                                                            |
-///                         |  ; Java 0: Iload_0                                         |
-///                         |  v0 = load.i32 notrap aligned l0              stack: [v0]  |
-///                         |                                                            |
-///                         |  ; Java 1: Iload_1                                         |
-///                         |  v1 = load.i32 notrap aligned l1          stack: [v0, v1]  |
-///                         |                                                            |
-///                         |  ; Java 2: If_icmplt(5)                                    |
-///                         |  v2 = icmp slt v0, v1                           stack: []  |
-///                         |  brif v2, block1, block2                        stack: []  |
-///                         +------------------------------------------------------------+
+///                         ╭────────────────────────────────────────────────────────────╮
+///                         │                         block0 ()                          │
+///                         │  // entry point with 2 local variables (l0: i32, l1: i32)  │
+///                         │                                                            │
+///                         │  ; Java 0: Iload_0                                         │
+///                         │  v0 = load.i32 notrap aligned l0              stack: [v0]  │
+///                         │                                                            │
+///                         │  ; Java 1: Iload_1                                         │
+///                         │  v1 = load.i32 notrap aligned l1          stack: [v0, v1]  │
+///                         │                                                            │
+///                         │  ; Java 2: If_icmplt(5)                                    │
+///                         │  v2 = icmp slt v0, v1                     stack: [v0, v1]  │
+///                         │  brif v2, block1, block2                  stack: [v0, v1]  │
+///                         ╰────────────────────────────────────────────────────────────╯
 ///                                           /                        \
 ///                                     (cond == 1)                (cond == 0)
 ///                                         /                            \
-///  +--------------------------------------------------+    +--------------------------------------------------+
-///  |                    block1 ()                     |    |                    block2 ()                     |
-///  |  // target of Java If_icmplt(5)                  |    |  // fall-through path to instructions 3 & 4      |
-///  |                                                  |    |                                                  |
-///  |  ; Java 5: Iload_1                               |    |  ; Java 3: Iload_0                               |
-///  |  v3 = load.i32 notrap aligned l1    stack: [v3]  |    |  v4 = load.i32 notrap aligned l0    stack: [v4]  |
-///  |                                                  |    |                                                  |
-///  |  jump block3(v3)                    stack: [v3]  |    |  ; Java 4: Goto 6                                |
-///  +--------------------------------------------------+    |  jump block3(v4)                    stack: [v4]  |
-///                                         \                +--------------------------------------------------+
+///  ╭──────────────────────────────────────────────────╮    ╭──────────────────────────────────────────────────╮
+///  │                    block1 ()                     │    │                    block2 ()                     │
+///  │  // target of Java If_icmplt(5)                  │    │  // fall-through path to instructions 3 & 4      │
+///  │                                                  │    │                                                  │
+///  │  ; Java 5: Iload_1                               │    │  ; Java 3: Iload_0                               │
+///  │  v3 = load.i32 notrap aligned l1    stack: [v3]  │    │  v4 = load.i32 notrap aligned l0    stack: [v4]  │
+///  │                                                  │    │                                                  │
+///  │  jump block3(v3)                    stack: [v3]  │    │  ; Java 4: Goto 6                                │
+///  ╰──────────────────────────────────────────────────╯    │  jump block3(v4)                    stack: [v4]  │
+///                                         \                ╰──────────────────────────────────────────────────╯
 ///                                          \                           /
 ///                                           \                         /
 ///                                            \                       /
 ///                                             v                     v
-///                              +--------------------------------------------------+
-///                              |             block3 (p0: i32)                     |
-///                              |  // join point with 1 incoming i32 param         |
-///                              |  v5 = load.i32 notrap aligned p0    stack: [v5]  |
-///                              |                                                  |
-///                              |  ; Java 6: Ireturn                               |
-///                              |  return v5                            stack: []  |
-///                              +--------------------------------------------------+
+///                              ╭──────────────────────────────────────────────────╮
+///                              │             block3 (p0: i32)                     │
+///                              │  // join point with 1 incoming i32 param         │
+///                              │  v5 = load.i32 notrap aligned p0    stack: [v5]  │
+///                              │                                                  │
+///                              │  ; Java 6: Ireturn                               │
+///                              │  return v5                            stack: []  │
+///                              ╰──────────────────────────────────────────────────╯
 /// ```
 ///
 mod blocks;
