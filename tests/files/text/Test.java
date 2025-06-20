d1014a3307575283fb7983da1fc2fa8b0ba50355
brianheineman <brian.heineman@gmail.com>
2025-06-20 16:56:35 -0600

    chore: update to cranelift=0.121.0, syn=2.0.104

--- tests/files/text/Test.java
+++ tests/files/text/Test.java
@@ -1,17 +1,17 @@
 import java.io.*;
-import java.nio.file.*;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         String filename = "test.txt";
+        File file = new File(filename);
         String content = "Hello, world!\nThis is a test file.";
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             writer.write(content);
         }
 
         StringBuilder sb = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 sb.append(line).append('\n');
@@ -20,8 +20,8 @@ public class Test {
         System.out.println("Read content:");
         System.out.println(sb);
 
-//         Files.delete(Paths.get(filename));
-//         System.out.print("File deleted: ");
-//         System.out.println(!Files.exists(Paths.get(filename)));
+        file.delete();
+        System.out.print("File deleted: ");
+        System.out.println(!file.exists());
     }
 }
