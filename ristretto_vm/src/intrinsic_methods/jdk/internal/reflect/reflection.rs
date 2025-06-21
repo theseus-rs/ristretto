c64264c8cf974ba6fee321bed6142f554ba2f53e
brianheineman <brian.heineman@gmail.com>
2025-06-21 13:35:28 -0600

    feat: implement jdk/internal/reflect/Reflection.areNestMates(Ljava/lang/Class;Ljava/lang/Class;)Z

--- ristretto_vm/src/intrinsic_methods/jdk/internal/reflect/reflection.rs
+++ ristretto_vm/src/intrinsic_methods/jdk/internal/reflect/reflection.rs
@@ -1,12 +1,14 @@
 use crate::Error::InternalError;
 use crate::Result;
+use crate::intrinsic_methods::java::lang::class;
 use crate::java_object::JavaObject;
 use crate::parameters::Parameters;
 use crate::thread::Thread;
 use async_recursion::async_recursion;
 use ristretto_classfile::JAVA_11;
 use ristretto_classfile::VersionSpecification::GreaterThanOrEqual;
-use ristretto_classloader::{Reference, Value};
+use ristretto_classfile::attributes::Attribute;
+use ristretto_classloader::{Class, Reference, Value};
 use ristretto_macros::intrinsic_method;
 use std::sync::Arc;
 
@@ -16,10 +18,48 @@ use std::sync::Arc;
 )]
 #[async_recursion(?Send)]
 pub(crate) async fn are_nest_mates(
-    _thread: Arc<Thread>,
-    _parameters: Parameters,
+    thread: Arc<Thread>,
+    mut parameters: Parameters,
 ) -> Result<Option<Value>> {
-    todo!("jdk.internal.reflect.Reflection.areNestMates(Ljava/lang/Class;Ljava/lang/Class;)Z")
+    let member_class_reference = parameters.pop_reference()?;
+    let Some(Reference::Object(member_class_object)) = member_class_reference else {
+        return Ok(Some(Value::from(false)));
+    };
+    let current_class_reference = parameters.pop_reference()?;
+    let Some(Reference::Object(current_class_object)) = current_class_reference else {
+        return Ok(Some(Value::from(false)));
+    };
+
+    let member_class = class::get_class(&thread, &member_class_object).await?;
+    let current_class = class::get_class(&thread, &current_class_object).await?;
+    if member_class == current_class {
+        return Ok(Some(Value::from(true)));
+    }
+
+    let member_nest_host = get_nest_host(&thread, member_class).await?;
+    let current_nest_host = get_nest_host(&thread, current_class).await?;
+    let are_nest_mates = member_nest_host == current_nest_host;
+    Ok(Some(Value::from(are_nest_mates)))
+}
+
+/// Returns the nest host class if it exists, otherwise returns the class.
+async fn get_nest_host(thread: &Arc<Thread>, class: Arc<Class>) -> Result<Arc<Class>> {
+    let class_file = class.class_file();
+    for attribute in &class_file.attributes {
+        let Attribute::NestHost {
+            name_index: _name_index,
+            host_class_index,
+        } = attribute
+        else {
+            continue;
+        };
+
+        let constant_pool = &class_file.constant_pool;
+        let host_class = constant_pool.try_get_class(*host_class_index)?;
+        let host_class = thread.class(host_class).await?;
+        return Ok(host_class);
+    }
+    Ok(class)
 }
 
 #[intrinsic_method(
@@ -76,12 +116,88 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    #[should_panic(
-        expected = "not yet implemented: jdk.internal.reflect.Reflection.areNestMates(Ljava/lang/Class;Ljava/lang/Class;)Z"
-    )]
-    async fn test_are_nest_mates() {
-        let (_vm, thread) = crate::test::thread().await.expect("thread");
-        let _ = are_nest_mates(thread, Parameters::default()).await;
+    async fn test_are_nest_mates_current_class_null_is_false() -> Result<()> {
+        let (vm, thread) = crate::test::thread().await.expect("thread");
+        let current_class = Value::Object(None);
+        let member_class = thread
+            .class("java.lang.String")
+            .await?
+            .to_object(&vm)
+            .await?;
+        let mut parameters = Parameters::default();
+        parameters.push(current_class);
+        parameters.push(member_class);
+        let value = are_nest_mates(thread, parameters)
+            .await?
+            .expect("nest_mates");
+        let are_nest_mates: bool = value.try_into()?;
+        assert!(!are_nest_mates);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_are_nest_mates_member_class_null_is_false() -> Result<()> {
+        let (vm, thread) = crate::test::thread().await.expect("thread");
+        let current_class = thread
+            .class("java.lang.String")
+            .await?
+            .to_object(&vm)
+            .await?;
+        let member_class = Value::Object(None);
+        let mut parameters = Parameters::default();
+        parameters.push(current_class);
+        parameters.push(member_class);
+        let value = are_nest_mates(thread, parameters)
+            .await?
+            .expect("nest_mates");
+        let are_nest_mates: bool = value.try_into()?;
+        assert!(!are_nest_mates);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_are_nest_mates_same_class_is_true() -> Result<()> {
+        let (vm, thread) = crate::test::thread().await.expect("thread");
+        let string_class = thread
+            .class("java.lang.String")
+            .await?
+            .to_object(&vm)
+            .await?;
+        let current_class = string_class.clone();
+        let member_class = string_class;
+        let mut parameters = Parameters::default();
+        parameters.push(current_class);
+        parameters.push(member_class);
+        let value = are_nest_mates(thread, parameters)
+            .await?
+            .expect("nest_mates");
+        let are_nest_mates: bool = value.try_into()?;
+        assert!(are_nest_mates);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_are_nest_mates_same_nest_host_is_true() -> Result<()> {
+        let (vm, thread) = crate::test::thread().await.expect("thread");
+        let current_class = thread
+            .class("java.lang.Integer")
+            .await?
+            .to_object(&vm)
+            .await?;
+        let member_class = thread
+            .class("java.lang.Integer$IntegerCache")
+            .await?
+            .to_object(&vm)
+            .await?;
+        let mut parameters = Parameters::default();
+        parameters.push(current_class);
+        parameters.push(member_class);
+        let value = are_nest_mates(thread, parameters)
+            .await?
+            .expect("nest_mates");
+        let are_nest_mates: bool = value.try_into()?;
+        assert!(are_nest_mates);
+        Ok(())
     }
 
     #[tokio::test]
