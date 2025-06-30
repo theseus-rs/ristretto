6559ea13068ac7a00b8c6cf39932f354a267b108
brianheineman <brian.heineman@gmail.com>
2025-06-30 16:13:35 -0600

    test: add method integration tests

---
+++ tests/method/override/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+    static class Animal {
+        public void speak() {
+            System.out.println("Animal speaks");
+        }
+    }
+
+    static class Dog extends Animal {
+        @Override
+        public void speak() {
+            System.out.println("Dog barks");
+        }
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Animal animalDog = new Dog();
+
+        animal.speak();
+        dog.speak();
+        animalDog.speak();
+    }
+}
