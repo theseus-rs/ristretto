31a826f3d16dd6390f0c6614485eb3cd49a8d670
brianheineman <brian.heineman@gmail.com>
2026-04-19 19:50:40 -0600

    feat: implement sun/rmi/transport/GC.maxObjectInspectionAge()J

--- ristretto_intrinsics/src/sun/rmi/transport/gc.rs
+++ ristretto_intrinsics/src/sun/rmi/transport/gc.rs
@@ -3,7 +3,6 @@ use ristretto_classfile::VersionSpecification::GreaterThanOrEqual;
 use ristretto_classloader::Value;
 use ristretto_macros::async_method;
 use ristretto_macros::intrinsic_method;
-use ristretto_types::JavaError;
 use ristretto_types::Thread;
 use ristretto_types::{Parameters, Result};
 use std::sync::Arc;
@@ -17,10 +16,7 @@ pub async fn max_object_inspection_age<T: Thread + 'static>(
     _thread: Arc<T>,
     _parameters: Parameters,
 ) -> Result<Option<Value>> {
-    Err(JavaError::UnsatisfiedLinkError(
-        "sun.rmi.transport.GC.maxObjectInspectionAge()J".to_string(),
-    )
-    .into())
+    Ok(Some(Value::Long(0)))
 }
 
 #[cfg(test)]
@@ -28,9 +24,10 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn test_max_object_inspection_age() {
-        let (_vm, thread) = crate::test::thread().await.expect("thread");
-        let result = max_object_inspection_age(thread, Parameters::default()).await;
-        assert!(result.is_err());
+    async fn test_max_object_inspection_age() -> Result<()> {
+        let (_vm, thread) = crate::test::java11_thread().await?;
+        let result = max_object_inspection_age(thread, Parameters::default()).await?;
+        assert_eq!(result, Some(Value::Long(0)));
+        Ok(())
     }
 }
