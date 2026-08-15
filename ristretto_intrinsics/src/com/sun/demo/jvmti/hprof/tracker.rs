615ebe59baf2347e0cb6d52311480c0e24dbdb19
brianheineman <brian.heineman@gmail.com>
2026-08-15 15:01:25 -0600

    feat: implement com/sun/demo/jvmti/hprof/Tracker

--- ristretto_intrinsics/src/com/sun/demo/jvmti/hprof/tracker.rs
+++ ristretto_intrinsics/src/com/sun/demo/jvmti/hprof/tracker.rs
@@ -3,10 +3,54 @@ use ristretto_classfile::VersionSpecification::LessThanOrEqual;
 use ristretto_classloader::Value;
 use ristretto_macros::async_method;
 use ristretto_macros::intrinsic_method;
-use ristretto_types::JavaError;
 use ristretto_types::Thread;
-use ristretto_types::{Parameters, Result};
+use ristretto_types::{Parameters, Result, VM as _};
 use std::sync::Arc;
+use std::sync::Mutex;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum HprofEventKind {
+    Call,
+    NewArray,
+    ObjectInit,
+    Return,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct HprofEvent {
+    kind: HprofEventKind,
+    object: usize,
+    related: usize,
+    class_index: i32,
+    trace_index: i32,
+}
+
+#[derive(Debug, Default)]
+struct HprofTrackerState(Mutex<Vec<HprofEvent>>);
+
+fn tracker_state<T: Thread + 'static>(thread: &T) -> Result<Arc<HprofTrackerState>> {
+    thread
+        .vm()?
+        .resource_manager()
+        .get_or_init(HprofTrackerState::default)
+}
+
+fn reference_identity(
+    reference: Option<
+        ristretto_gc::Gc<ristretto_gc::sync::RwLock<ristretto_classloader::Reference>>,
+    >,
+) -> usize {
+    reference.map_or(0, |reference| reference.as_ptr() as usize)
+}
+
+fn record<T: Thread + 'static>(thread: &T, event: HprofEvent) -> Result<()> {
+    tracker_state(thread)?
+        .0
+        .lock()
+        .unwrap_or_else(std::sync::PoisonError::into_inner)
+        .push(event);
+    Ok(())
+}
 
 #[intrinsic_method(
     "com/sun/demo/jvmti/hprof/Tracker.nativeCallSite(Ljava/lang/Object;II)V",
@@ -14,16 +58,23 @@ use std::sync::Arc;
 )]
 #[async_method]
 pub async fn native_call_site<T: Thread + 'static>(
-    _thread: Arc<T>,
+    thread: Arc<T>,
     mut parameters: Parameters,
 ) -> Result<Option<Value>> {
-    let _arg2 = parameters.pop_int()?;
-    let _arg1 = parameters.pop_int()?;
-    let _arg0 = parameters.pop_reference()?;
-    Err(JavaError::UnsatisfiedLinkError(
-        "com.sun.demo.jvmti.hprof.Tracker.nativeCallSite(Ljava/lang/Object;II)V".to_string(),
-    )
-    .into())
+    let trace_index = parameters.pop_int()?;
+    let class_index = parameters.pop_int()?;
+    let object = reference_identity(parameters.pop_reference()?);
+    record(
+        thread.as_ref(),
+        HprofEvent {
+            kind: HprofEventKind::Call,
+            object,
+            related: 0,
+            class_index,
+            trace_index,
+        },
+    )?;
+    Ok(None)
 }
 
 #[intrinsic_method(
@@ -32,16 +83,22 @@ pub async fn native_call_site<T: Thread + 'static>(
 )]
 #[async_method]
 pub async fn native_new_array<T: Thread + 'static>(
-    _thread: Arc<T>,
+    thread: Arc<T>,
     mut parameters: Parameters,
 ) -> Result<Option<Value>> {
-    let _arg1 = parameters.pop_reference()?;
-    let _arg0 = parameters.pop_reference()?;
-    Err(JavaError::UnsatisfiedLinkError(
-        "com.sun.demo.jvmti.hprof.Tracker.nativeNewArray(Ljava/lang/Object;Ljava/lang/Object;)V"
-            .to_string(),
-    )
-    .into())
+    let related = reference_identity(parameters.pop_reference()?);
+    let object = reference_identity(parameters.pop_reference()?);
+    record(
+        thread.as_ref(),
+        HprofEvent {
+            kind: HprofEventKind::NewArray,
+            object,
+            related,
+            class_index: 0,
+            trace_index: 0,
+        },
+    )?;
+    Ok(None)
 }
 
 #[intrinsic_method(
@@ -50,16 +107,22 @@ pub async fn native_new_array<T: Thread + 'static>(
 )]
 #[async_method]
 pub async fn native_object_init<T: Thread + 'static>(
-    _thread: Arc<T>,
+    thread: Arc<T>,
     mut parameters: Parameters,
 ) -> Result<Option<Value>> {
-    let _arg1 = parameters.pop_reference()?;
-    let _arg0 = parameters.pop_reference()?;
-    Err(JavaError::UnsatisfiedLinkError(
-        "com.sun.demo.jvmti.hprof.Tracker.nativeObjectInit(Ljava/lang/Object;Ljava/lang/Object;)V"
-            .to_string(),
-    )
-    .into())
+    let related = reference_identity(parameters.pop_reference()?);
+    let object = reference_identity(parameters.pop_reference()?);
+    record(
+        thread.as_ref(),
+        HprofEvent {
+            kind: HprofEventKind::ObjectInit,
+            object,
+            related,
+            class_index: 0,
+            trace_index: 0,
+        },
+    )?;
+    Ok(None)
 }
 
 #[intrinsic_method(
@@ -68,16 +131,23 @@ pub async fn native_object_init<T: Thread + 'static>(
 )]
 #[async_method]
 pub async fn native_return_site<T: Thread + 'static>(
-    _thread: Arc<T>,
+    thread: Arc<T>,
     mut parameters: Parameters,
 ) -> Result<Option<Value>> {
-    let _arg2 = parameters.pop_int()?;
-    let _arg1 = parameters.pop_int()?;
-    let _arg0 = parameters.pop_reference()?;
-    Err(JavaError::UnsatisfiedLinkError(
-        "com.sun.demo.jvmti.hprof.Tracker.nativeReturnSite(Ljava/lang/Object;II)V".to_string(),
-    )
-    .into())
+    let trace_index = parameters.pop_int()?;
+    let class_index = parameters.pop_int()?;
+    let object = reference_identity(parameters.pop_reference()?);
+    record(
+        thread.as_ref(),
+        HprofEvent {
+            kind: HprofEventKind::Return,
+            object,
+            related: 0,
+            class_index,
+            trace_index,
+        },
+    )?;
+    Ok(None)
 }
 
 #[cfg(test)]
@@ -92,10 +162,7 @@ mod tests {
             Parameters::new(vec![Value::Object(None), Value::Int(0), Value::Int(0)]),
         )
         .await;
-        assert_eq!(
-            "com.sun.demo.jvmti.hprof.Tracker.nativeCallSite(Ljava/lang/Object;II)V",
-            result.unwrap_err().to_string()
-        );
+        assert_eq!(None, result.expect("result"));
     }
 
     #[tokio::test]
@@ -106,10 +173,7 @@ mod tests {
             Parameters::new(vec![Value::Object(None), Value::Object(None)]),
         )
         .await;
-        assert_eq!(
-            "com.sun.demo.jvmti.hprof.Tracker.nativeNewArray(Ljava/lang/Object;Ljava/lang/Object;)V",
-            result.unwrap_err().to_string()
-        );
+        assert_eq!(None, result.expect("result"));
     }
 
     #[tokio::test]
@@ -120,10 +184,7 @@ mod tests {
             Parameters::new(vec![Value::Object(None), Value::Object(None)]),
         )
         .await;
-        assert_eq!(
-            "com.sun.demo.jvmti.hprof.Tracker.nativeObjectInit(Ljava/lang/Object;Ljava/lang/Object;)V",
-            result.unwrap_err().to_string()
-        );
+        assert_eq!(None, result.expect("result"));
     }
 
     #[tokio::test]
@@ -134,9 +195,6 @@ mod tests {
             Parameters::new(vec![Value::Object(None), Value::Int(0), Value::Int(0)]),
         )
         .await;
-        assert_eq!(
-            "com.sun.demo.jvmti.hprof.Tracker.nativeReturnSite(Ljava/lang/Object;II)V",
-            result.unwrap_err().to_string()
-        );
+        assert_eq!(None, result.expect("result"));
     }
 }
